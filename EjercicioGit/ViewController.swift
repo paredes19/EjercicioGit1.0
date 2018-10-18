40957f8ae8cd37bd7db86db4e45e2ec5981d810a
Virtualizacion 02 <virtualizacion02@virtualizacions-imac-3.local>
2018-10-18 18:36:27 -0500

    Initial Commit

---
+++ EjercicioGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EjercicioGit
+//
+//  Created by Virtualizacion 02 on 10/18/18.
+//  Copyright © 2018 UIA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
