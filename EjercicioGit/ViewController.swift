720f471f063fb2368d0d7f61d3bd39940fbca463
Virtualizacion 02 <virtualizacion02@Virtualizacions-iMac-3.local>
2018-10-18 19:01:06 -0500

    Cambia color

--- EjercicioGit/ViewController.swift
+++ EjercicioGit/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var contador: UILabel!
+    
+    var cont = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,9 +25,28 @@ class ViewController: UIViewController {
     }
 
     @IBAction func aumentaContador(_ sender: Any) {
-        var cont = 0
-        cont += 1
+        cont = cont + 1
         contador.text = String(cont)
+        
+        if cont % 20 == 0{
+            let nuevoColor = Int(arc4random_uniform(5))
+            
+            switch nuevoColor{
+            case 0:
+                contador.textColor = UIColor(red: 1, green: 0.5, blue: 0.1, alpha: 0.7)
+            case 1:
+                contador.textColor = UIColor(red: 0.1, green: 1, blue: 0.9, alpha: 0.7)
+            case 2:
+                contador.textColor = UIColor(red: 0.7, green: 0.9, blue: 1, alpha: 0.7)
+            case 3:
+                contador.textColor = UIColor(red: 0.3, green: 0.7, blue: 0.1, alpha: 0.7)
+            case 4:
+                contador.textColor = UIColor(red: 0.1, green: 0.3, blue: 0.7, alpha: 0.7)
+            case 5:
+                contador.textColor = UIColor(red: 0.7, green: 0.1, blue: 0.3, alpha: 0.7)
+            default: break
+            }
+        }
     }
     
 }
